59a1ff77502ab57efa1c86c724cc6d19f82d976f
d36choi <40600306+d36choi@users.noreply.github.com>
2020-08-25 14:25:31 +0900

    로그인은 가능한데 [authorization_request_not_found] 뜸

--- src/main/java/com/hanium/hfrecruit/config/WebSecurityConfig.java
+++ src/main/java/com/hanium/hfrecruit/config/WebSecurityConfig.java
@@ -1,46 +1,54 @@
 package com.hanium.hfrecruit.config;
 
-import com.hanium.hfrecruit.oauth2.CustomOAuthProvider;
-import lombok.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *  https://momentjin.tistory.com/144
  */
-
-@Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // disables cors and csrf
+        http
+                .cors().and()
+                .csrf()
+                .disable();
+
+        // authenticate
         http.authorizeRequests()
+                .antMatchers("/my").permitAll()
+                .antMatchers("/").permitAll()
                 .antMatchers("/oauth2/**").permitAll()
+                .antMatchers("/login/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
+//                .userInfoEndpoint()
+//                .customUserType(KakaoOAuth2User.class, "kakao").and()
+//                .successHandler(oAuth2SuccessHandler);
+
+        // disables session creation on Spring Security
+//        http.sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository (
-            @Value("${spring.security.oauth2.client.registration.kakao.client-id}") String kakaoClientId,
-            @Value("${spring.security.oauth2.client.registration.kakao.client-secret}") String kakaoClientSecret
-    ) {
-        List<ClientRegistration> registrations = new ArrayList<>();
-        registrations.add(CustomOAuthProvider.KAKAO.getBuilder().clientId(kakaoClientId)
-        .clientSecret(kakaoClientSecret).jwkSetUri("temp").build());
-
-        return new InMemoryClientRegistrationRepository(registrations);
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
 }
--- src/main/java/com/hanium/hfrecruit/oauth2/kakao/CustomOAuthProvider.java
+++ src/main/java/com/hanium/hfrecruit/oauth2/kakao/CustomOAuthProvider.java
@@ -1,23 +1,27 @@
-package com.hanium.hfrecruit.oauth2;
+package com.hanium.hfrecruit.oauth2.kakao;
 
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 
+/**
+ * Created by momentjin@gmail.com on 2019-12-10
+ * Github : http://github.com/momentjin
+ */
 public enum CustomOAuthProvider {
 
     KAKAO {
         @Override
         public ClientRegistration.Builder getBuilder() {
             return getBuilder("kakao", ClientAuthenticationMethod.POST)
-                    .scope("profile", "talk_message") // 요청할 권한
-                    .authorizationUri("https://kauth.kakao.com/oauth/authorize") // authorization code 얻는 API
-                    .tokenUri("https://kauth.kakao.com/oauth/token") // access Token 얻는 API
-                    .userInfoUri("https://kapi.kakao.com/v2/user/me") // 유저 정보 조회 API
+                    .scope("profile", "talk_message")
+                    .authorizationUri("https://kauth.kakao.com/oauth/authorize")
+                    .tokenUri("https://kauth.kakao.com/oauth/token")
+                    .userInfoUri("https://kapi.kakao.com/v2/user/me")
                     .clientId("f9580fd7455b0683b9b55e0d431b1e00")
                     .clientSecret("G6hy1S3Z1xsnBiavVyXCYyRxdWa9kzh4")
-                    .userNameAttributeName("id") // userInfo API Response에서 얻어올 ID 프로퍼티
-                    .clientName("Kakao"); // spring 내에서 인식할 OAuth2 Provider Name
+                    .userNameAttributeName("id")
+                    .clientName("Kakao");
         }
     };
 
@@ -30,6 +34,7 @@ public enum CustomOAuthProvider {
         builder.clientAuthenticationMethod(method);
         builder.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE);
         builder.redirectUriTemplate(CustomOAuthProvider.DEFAULT_LOGIN_REDIRECT_URL);
+
         return builder;
     }
 
