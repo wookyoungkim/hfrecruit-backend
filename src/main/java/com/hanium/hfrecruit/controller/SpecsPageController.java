8e5d79e61c84273d90ccd375be9a9c25a5b9355b
d36choi <yuio1126@gmail.com>
2020-08-28 16:37:01 +0900

    sidebar를 layout화하여 분리. 각 메뉴의 controller 객체를 만듬.href 를 연결함. close #12

---
+++ src/main/java/com/hanium/hfrecruit/controller/SpecsPageController.java
@@ -0,0 +1,13 @@
+package com.hanium.hfrecruit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpecsPageController {
+    @GetMapping("/specs")
+    public String index(){
+
+        return "index";
+    }
+}
