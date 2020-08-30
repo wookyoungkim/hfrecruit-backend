14031c5d100f30f7741e9b84d2a399516d8a8da7
wookyoungkim <viliketh1s98@naver.com>
2020-08-31 04:40:42 +0900

    user class의 email 추가 (#20) -> 소셜로그인 위한 변수들 추가 google, naver 소셜로그인 추가 (#1) -> db에 유저정보 추가되는것 확인

--- src/main/java/com/hanium/hfrecruit/domain/user/UserRepository.java
+++ src/main/java/com/hanium/hfrecruit/domain/user/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserNo(long userNo);
+
+    Optional<User> findByEmail(String email);
 }
