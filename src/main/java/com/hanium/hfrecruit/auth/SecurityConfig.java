ba345d4c85b4304204fb154853a2a33aa98dccf3
wookyoungkim <viliketh1s98@naver.com>
2020-08-31 04:48:16 +0900

    google, naver 소셜로그인 추가 (#1) db에 유저정보 추가되는것 확인

---
+++ src/main/java/com/hanium/hfrecruit/auth/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.hanium.hfrecruit.auth;
+
+import com.hanium.hfrecruit.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().headers().frameOptions().disable() // (2)
+                .and()
+                .authorizeRequests() // (3)
+                .antMatchers("/", "/css/**","/images/**","/js/**","/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated() // (5)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/home")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+//    private ClientRegistration getRegistration(OAuth2ClientProperties clientProperties, String client) {
+//        if ("google".equals(client)) {
+//            OAuth2ClientProperties.Registration registration = clientProperties.getRegistration().get("google");
+//            return CommonOAuth2Provider.GOOGLE.getBuilder(client)
+//                    .clientId(registration.getClientId())
+//                    .clientSecret(registration.getClientSecret())
+//                    .scope("email", "profile")
+//                    .build();
+//        }
+//
+//        return null;
+//    }
+}
