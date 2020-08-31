e04135b9cb7026a3b68fd18aaf6df29791b87ada
d36choi <yuio1126@gmail.com>
2020-08-31 21:27:36 +0900

    내 스펙 페이지 UI 구현함. close #24

--- src/main/java/com/hanium/hfrecruit/controller/SpecsPageController.java
+++ src/main/java/com/hanium/hfrecruit/controller/SpecsPageController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SpecsPageController {
     @GetMapping("/specs")
     public String index(){
-        return "index";
+
+        return "myspec";
     }
 }
