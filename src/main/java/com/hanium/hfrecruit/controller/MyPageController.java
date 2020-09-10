662ac61180c265e2811ccf06ce92f463540457bb
wookyoungkim <viliketh1s98@naver.com>
2020-09-11 01:31:37 +0900

    /mypage 유저정보 수정 기능 구현 closes #54

--- src/main/java/com/hanium/hfrecruit/controller/MyPageController.java
+++ src/main/java/com/hanium/hfrecruit/controller/MyPageController.java
@@ -1,20 +1,48 @@
 package com.hanium.hfrecruit.controller;
 
+import com.hanium.hfrecruit.auth.dto.SessionUser;
+import com.hanium.hfrecruit.domain.user.User;
+import com.hanium.hfrecruit.domain.user.UserRepository;
+import com.hanium.hfrecruit.dto.UserUpdateRequestDto;
+import com.hanium.hfrecruit.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.NoResultException;
+import javax.servlet.http.HttpSession;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping
 public class MyPageController {
+    private final HttpSession httpSession;
+    @Autowired
+    private UserService userService;
+    private final UserRepository userRepository;
+
+
     @GetMapping("/mypage")
     public String mypage(){
         return "mypage";
     }
 
     @GetMapping("/profile")
-    public String profile(){
+    public String profile(@SessionAttribute("user") SessionUser sessionUser, Model model){
+        User user = userRepository.findByEmail(sessionUser.getEmail()).orElseThrow(
+                () -> new IllegalArgumentException("finding userNo Failed!")
+        );
+        model.addAttribute("user", user);
+
         return "profile";
+    }
+
+    @PutMapping("/mypage/{userNo}")
+    @ResponseBody
+    public Long update(@PathVariable Long userNo, @RequestBody UserUpdateRequestDto requestDto){
+        return userService.update(userNo, requestDto);
     }
 
     @GetMapping("/evaluate")
--- src/main/resources/static/js/app/userInfo.js
+++ src/main/resources/static/js/app/userInfo.js
@@ -1,8 +1,26 @@
 var userInfo = {
     init : function () {
         var _this = this;
+        'use strict';
+        // window.addEventListener('load', function() {
+        //     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        //     var forms = document.getElementsByClassName('needs-validation');
+        //     // Loop over them and prevent submission
+        //     var validation = Array.prototype.filter.call(forms, function(form) {
+        //         form.addEventListener('submit', function(event) {
+        //             if (form.checkValidity() === false) {
+        //                 event.preventDefault();
+        //                 event.stopPropagation();
+        //             }
+        //             form.classList.add('was-validated');
+        //         }, false);
+        //     });
+        // }, false);
         $('#btn-userInfo-save').on('click', function (){
             _this.save();
+        });
+        $('#btn-userInfo-update').on('click', function (){
+            _this.update();
         });
     },
     save : function (){
@@ -35,6 +53,39 @@ var userInfo = {
             alert('회원가입이 완료되었습니다.');
             window.location.href = '/';
         }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function () {
+        var check_count = document.getElementsByName("gender").length;
+        var gender = 0;
+        for (var i=0; i<check_count; i++) {
+            if (document.getElementsByName("gender")[i].checked == true) {
+                gender = document.getElementsByName("gender")[i].value;
+            }
+        }
+        var data = {
+            username: $('#username').val(),
+            birth: $('#birth').val(),
+            address: $('#address').val(),
+            college: $('#college').val(),
+            highschool: $('#highschool').val(),
+            educationLevel: $('#educationLevel').val(),
+            militaryService: $('#militaryService').val(),
+            gender: gender
+        };
+        var userNo = $('#userNo').val();
+        console.log(data);
+        $.ajax({
+            type: 'PUT',
+            url: '/mypage/' + userNo,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('회원정보 수정이 완료되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     }
