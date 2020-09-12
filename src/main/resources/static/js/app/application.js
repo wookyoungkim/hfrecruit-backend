3c9d3c63084df59ce0b39cdf165dce552af4963c
wookyoungkim <viliketh1s98@naver.com>
2020-09-12 22:54:02 +0900

    지원서 수정시 공백생기는거 수정 closes #50

--- src/main/resources/static/js/app/application.js
+++ src/main/resources/static/js/app/application.js
@@ -33,7 +33,7 @@ var main = {
             data : JSON.stringify(data)
         }).done(function () {
             alert('글이 등록되었습니다.');
-            window.location.href = '/mypage';
+            window.location.href = '/application/list';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         })
