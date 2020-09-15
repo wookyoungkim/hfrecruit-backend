59cb85a6140b3513cee766fe7e63e6f1e19b191c
d36choi <yuio1126@gmail.com>
2020-09-15 21:57:38 +0900

    about page 생성 및 컨트롤러 매핑

--- src/main/java/com/hanium/hfrecruit/controller/AboutPageController.java
+++ src/main/java/com/hanium/hfrecruit/controller/AboutPageController.java
@@ -1,12 +1,14 @@
 package com.hanium.hfrecruit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AboutPageController {
     @GetMapping("/about")
-    public String About(){
-        return "index";
+    public String About(Model model){
+        model.addAttribute("pageTitle","About");
+        return "about";
     }
 }
