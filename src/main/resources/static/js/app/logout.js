5609ff6c482bde0d07f0ae8a4404e669539f94ea
wookyoungkim <viliketh1s98@naver.com>
2020-09-21 10:50:22 +0900

    회원탈퇴 기능 위한 userstate 비트 추가

--- src/main/resources/static/js/app/logout.js
+++ src/main/resources/static/js/app/logout.js
@@ -6,11 +6,32 @@ var main = {
         });
         $('#btn-logoutbar').on('click', function () {
             _this.logout();
-        })
+        });
+        $('#btn-deletebar').on('click', function () {
+            _this.withdrawal();
+        });
+        $('#btn-deleteUser').on('click', function () {
+            _this.withdrawal();
+        });
+
     },
     logout : function () {
         if(confirm("로그아웃 하시겠습니까?")) {
             window.location.href = "/logout";
+        }
+    },
+    withdrawal : function () {
+
+        if(confirm("정말로 회원 탈퇴 하시겠습니까?")) {
+            $.ajax({
+                type : 'PUT',
+                url : '/user/delete'
+            }).done(function () {
+                alert('탈퇴 완료 되었습니다.');
+                window.location.href = '/logout';
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            })
         }
     }
 };
