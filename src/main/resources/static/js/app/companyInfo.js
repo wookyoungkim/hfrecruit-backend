cf4d69a6581216dfcb66c2ee2ca549539c1787a2
dusdn1702 <dusdn1702@naver.com>
2020-09-25 01:45:06 +0900

    기업 정보 수정 완료 close #97
    topnavbar 글씨 조금 작게 수정

--- src/main/resources/static/js/app/companyInfo.js
+++ src/main/resources/static/js/app/companyInfo.js
@@ -6,6 +6,11 @@ var companyInfo = {
                 _this.save();
             }
         });
+        $('#btn-companyInfo-update').on('click', function () {
+            if(_this.check()===true) {
+                _this.update();
+            }
+        });
     },
     check: function () {
         if (document.getElementById("gridCheck").checked === false) {
@@ -23,7 +28,6 @@ var companyInfo = {
             managerId: $('#managerId').val(),
             companyLogo: $('#companyLogo').val()
         };
-
         $.ajax({
             type: 'POST',
             url: '/companyInfo/save',
@@ -33,6 +37,28 @@ var companyInfo = {
         }).done(function () {
             alert('기업등록이 완료되었습니다.');
             window.location.href = '/companyUser';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update: function () {
+        var data = {
+            companyName: $('#companyName').val(),
+            companyEmail: $('#companyEmail').val(),
+            companyPage: $('#companyPage').val(),
+            managerId: $('#managerId').val(),
+            companyLogo: $('#companyLogo').val()
+        };
+        var companyNo = $('#companyNo').val()
+        $.ajax({
+            type: 'PUT',
+            url: '/mypage/company-info-update/'+companyNo,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('기업 수정이 완료되었습니다.');
+            window.location.href = '/mypage';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
